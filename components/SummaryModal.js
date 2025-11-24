28000d08d7fdd92f9bfe95c942f59ce3d980ef5e
Aarsh <aarshps@gmail.com>
2025-11-24 19:37:24 +0530

    Fix build: remove duplicate React import in SummaryModal

--- components/SummaryModal.js
+++ components/SummaryModal.js
@@ -1,5 +1,4 @@
 import React, { useMemo } from 'react';
-import React, { useMemo } from 'react';
 import Modal from './Modal';
 import Button from './Button';
 import { COLORS } from '../lib/colors';
