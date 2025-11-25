6338cb6fb0ee77606c468d204a84883c4e64a3b0
Aarsh <aarshps@gmail.com>
2025-11-25 17:49:05 +0530

    Feat: Add Family category

--- components/SubscriptionForm.js
+++ components/SubscriptionForm.js
@@ -15,7 +15,7 @@ const BILLING_CYCLES = [
     { value: 'daily', label: 'Daily' },
     { value: 'custom', label: 'Custom (Days)' },
 ];
-export const CATEGORIES = ['Entertainment', 'Utilities', 'Software', 'Social', 'Insurance', 'Gym/Fitness', 'Investment', 'Loan/EMI', 'Other'];
+export const CATEGORIES = ['Entertainment', 'Utilities', 'Software', 'Social', 'Family', 'Insurance', 'Gym/Fitness', 'Investment', 'Loan/EMI', 'Other'];
 
 const SubscriptionForm = ({
     initialName = '',
