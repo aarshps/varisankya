bae48a476e2b614e7d6bfc3df79efb682344c7fd
Aarsh <aarshps@gmail.com>
2025-11-26 17:18:54 +0530

    fix: restore webDir to public to fix capacitor sync error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
     appId: 'com.varisankya.app',
     appName: 'Varisankya',
-    webDir: 'out',
+    webDir: 'public',
     plugins: {
         CapacitorHttp: {
             enabled: true
