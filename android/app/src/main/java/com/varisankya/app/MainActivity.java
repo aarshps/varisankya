56609d21df94fc4f178c0725aba287e6e55feeca
Aarsh <aarshps@gmail.com>
2025-11-26 16:27:02 +0530

    fix: configure WebView to hide browser chrome and UI elements

--- android/app/src/main/java/com/varisankya/app/MainActivity.java
+++ android/app/src/main/java/com/varisankya/app/MainActivity.java
@@ -2,4 +2,27 @@ package com.varisankya.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        
+        // Configure WebView to hide any browser UI
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            android.webkit.WebView webView = this.bridge.getWebView();
+            android.webkit.WebSettings webSettings = webView.getSettings();
+            
+            // Set user agent to not trigger browser mode
+            String userAgent = webSettings.getUserAgentString();
+            userAgent = userAgent.replace("; wv", "");
+            webSettings.setUserAgentString(userAgent);
+            
+            // Disable any browser-like features
+            webSettings.setBuiltInZoomControls(false);
+            webSettings.setDisplayZoomControls(false);
+            
+            // Ensure it's treated as an app, not a browser
+            webSettings.setMixedContentMode(android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+    }
+}
