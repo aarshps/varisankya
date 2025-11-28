3caf960302d20949457f0dbb670d99e19cee4951
Aarsh <aarshps@gmail.com>
2025-11-28 18:52:52 +0530

    Style header with rounded edges and fix app name alignment

--- components/AppNameComponent.js
+++ components/AppNameComponent.js
@@ -3,7 +3,14 @@ import { COLORS } from '../lib/colors';
 
 export default function AppNameComponent() {
     return (
-        <span style={{ fontWeight: '500', fontSize: '20px', lineHeight: '36px', color: COLORS.textPrimary }}>
+        <span style={{
+            fontWeight: '500',
+            fontSize: '20px',
+            color: COLORS.textPrimary,
+            display: 'flex',
+            alignItems: 'center',
+            height: '100%'
+        }}>
             Varisankya
         </span>
     );
